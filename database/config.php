af76afa1037b8f734130507316df134a066e98e4
HooangF4t <110109210+HooangF4t@users.noreply.github.com>
2025-01-20 16:10:30 +0700

    Update config.php

--- database/config.php
+++ database/config.php
@@ -1,1 +1,5 @@
-chỗ này chứa cấu hình csdl
+<?
+define('HOST', 'localhost');
+define('DATABASE', 'pndshop');    #tên của database
+define('USERNAME', 'root ');     #sau này đẩy lên hệ thống sever thì thay vào
+define('PASSWORD', '');          #sau này đẩy lên hệ thống sever thì thay vào
